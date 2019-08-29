60805ba453e5aa742432a703afb38c536a99bae9
Socha <mattsocha11@gmail.com>
2019-08-28 22:44:31 -0400

    create login component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
     <ToDoApp/>
+    <modal name="login" width="85%" height="auto" class="customModal">
+      <Login/>
+    </modal>
   </div>
 </template>
 
 <script>
 import ToDoApp from './components/ToDoApp.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'app',
   components: {
-    ToDoApp
+    ToDoApp,
+    Login,
+  },
+  data() {
+    return {
+      showLogin: true,
+    }
+  },
+  mounted() {
+
+    this.$modal.show('login');
   }
 }
 </script>
@@ -23,5 +37,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.customModal {
+  padding: 20px;
+}
+.button {
+  color: white;
+  width: 100px;
+  height: 50px;
+  background-color: green;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="padding: 20px; margin: auto;">
+    Enter Your Name <br/>
+    <input type="text" name="fname"><br>
+    <div class="button"> Login</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
