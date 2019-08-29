07fcf53993df63fea33f6a919d570ce74bc2bc77
Socha <mattsocha11@gmail.com>
2019-08-29 00:43:49 -0400

    add buttons to item list

--- src/components/itemList.vue
+++ src/components/itemList.vue
@@ -2,13 +2,28 @@
   <div style="margin-top:25px;">
     <h2 v-if="items.length === 0">No Items In List</h2>
     <div v-for="item in items" class="card">
-      <div class="listItem">
-        <h4>Title: {{ item.title }}</h4>
-        <h4>Created By: {{ item.creator }}</h4>
+      <div class="topSide">
+        <div class="cardHeader">
+          <h4 style="margin:5px;">Title: {{ item.title }}</h4>
+          <span>Created By: {{ item.creator }}</span>
+        </div>
+        <div class="cardHeader">
+          <h4 style="margin:5px;">Description: {{ item.description }}</h4>
+          <h4 style="margin:5px;">Staus: {{ item.status }}</h4>
+        </div>
+        <div class="cardHeader">
+          <h4 style="margin:5px;">Due Date: {{ item.date | moment("dddd, MMMM Do YYYY") }}</h4>
+        </div>
       </div>
       <div class="listItem">
-        <h4>Description: {{ item.description }}</h4>
-        <h4>Staus: {{ item.status }}</h4>
+      </div>
+      <div class="listItem">
+        <div>
+          <input type="submit" style="width:100%;" value="Edit Item" v-on:click="showAddItemModal()">
+        </div>
+        <div>
+          <input type="submit" style="width:100%; background-color:red;" value="Delete Item" v-on:click="showAddItemModal()">
+        </div>
       </div>
     </div>
   </div>
@@ -39,13 +54,25 @@ export default {
 </script>
 
 <style scoped>
+
+.topSide {
+  justify-content: space-around;
+  display: flex;
+}
 .card {
+  margin: 20px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
 }
 
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.cardHeader {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 .listItem {
   display: flex;
